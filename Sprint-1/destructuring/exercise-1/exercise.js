50821750ab194e47424e1a82c8003ff664c7c6de
dona-shehu <shehu.donna@gmail.com>
2026-07-30 00:23:37 +0100

    Object destructuring

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -6,7 +6,8 @@ const personOne = {
 
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself() {
+  let {name,age,favouriteFood} = personOne;
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
