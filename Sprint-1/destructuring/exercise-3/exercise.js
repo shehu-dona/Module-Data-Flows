ec64189b84568d48de3d12976375b4e4d4e94983
dona-shehu <shehu.donna@gmail.com>
2026-07-31 00:08:16 +0100

    receipt() using destructuring, padEnd , toFixed

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,23 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+function receipt(order) {
+  let totalPrice = 0;
+
+  console.log("QTY".padEnd(10) + "Item".padEnd(20) + "TOTAL");
+  for (const item of order) {
+    let { itemName, quantity, unitPricePence } = item;
+    let pricePerQuantity = quantity * unitPricePence;
+    console.log(
+      String(quantity).padEnd(10) +
+        itemName.padEnd(20) +
+        (pricePerQuantity / 100).toFixed(2)
+    );
+    totalPrice += pricePerQuantity;
+  }
+  totalPrice = (totalPrice / 100).toFixed(2);
+  console.log(`TOTAL: ${totalPrice}`);
+}
+
+receipt(order);
